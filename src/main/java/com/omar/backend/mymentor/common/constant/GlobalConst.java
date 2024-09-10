4f023e5cb2a4805f876fd7fea2b27fb69ad5aaaa
Omar <omar.enrique.alvarez.garcia@gmail.com>
2024-09-10 16:06:35 -0300

    Se agrega una nueva constante

--- src/main/java/com/omar/backend/mymentor/common/constant/GlobalConst.java
+++ src/main/java/com/omar/backend/mymentor/common/constant/GlobalConst.java
@@ -2,6 +2,6 @@ package com.omar.backend.mymentor.common.constant;
 
 public class GlobalConst {
 
-    public static final String INSTRUCTIONS = " - Indicaciones importantes: 1 Que siempre responda en español.";
+    public static final String INSTRUCTIONS = " - Indicaciones importantes: 1 Que siempre responda en español. 2 Cada vez que respondas un código o algo que se quiera resaltar, retornalo encapsulado en bloques de código Markdown. ";
     // public static final String INSTRUCTIONS = " - Indicaciones importantes: 1 Que siempre hable en español. 2 Que nunca conteste preguntas fuera de tema. 3 En caso de que pregunten cosas fuera de su experiencia que lo derive con un especialista.";
 }
