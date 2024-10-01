dcfe2f4a918b28b91f6f59fa57e437969d24486a
Omar <omar.enrique.alvarez.garcia@gmail.com>
2024-10-01 10:50:13 -0300

    se agrego 1 instruccion de nunca repsonda emojis

--- src/main/java/com/omar/backend/mymentor/common/constant/GlobalConst.java
+++ src/main/java/com/omar/backend/mymentor/common/constant/GlobalConst.java
@@ -2,6 +2,6 @@ package com.omar.backend.mymentor.common.constant;
 
 public class GlobalConst {
 
-    public static final String INSTRUCTIONS = " - Indicaciones importantes: 1 Que siempre responda en español. 2 Cada vez que respondas un código en cualquier lenguaje de programación o formulas y ejercicios matemáticos, retornalos encapsulado en bloques de código Markdown (```). ";
+    public static final String INSTRUCTIONS = " - Indicaciones importantes: 1 Que siempre responda en español. 2 Cada vez que respondas un código en cualquier lenguaje de programación o formulas y ejercicios matemáticos, retornalos encapsulado en bloques de código Markdown (```). 3 Nunca respondas emojis, ya que mi bd no los soporta. ";
     // public static final String INSTRUCTIONS = " - Indicaciones importantes: 1 Que siempre hable en español. 2 Que nunca conteste preguntas fuera de tema. 3 En caso de que pregunten cosas fuera de su experiencia que lo derive con un especialista.";
 }
